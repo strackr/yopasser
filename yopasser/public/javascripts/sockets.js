cb82a7409ba4366110b916987e6393a27e643f30
Robert Strack <robert.strack@gmail.com>
2015-03-05 23:07:59 -0500

    sending messages

--- yopasser/public/javascripts/sockets.js
+++ yopasser/public/javascripts/sockets.js
@@ -1,6 +1,11 @@
-var socket = new WebSocket('ws://localhost:3000/comm')
+var host = location.origin.replace(/^http/, 'ws')
+var socket = new WebSocket(host + '/comm')
 socket.onopen = function() {
   console.log('connection opened');
+  $('#send').click(function() {
+    console.log('sending: Yo!');
+    socket.send('Yo!');
+  });
   $('#send').removeClass('disabled');
 }
 socket.onmessage = function(message) {
